e7fcdb6487a752781fb7e89cfc2f1fa697eec0f4
Phil Murphy <phillipdeanmurphy@gmail.com>
2021-05-08 16:27:23 -0700

    problem four has been solved

--- js/app.js
+++ js/app.js
@@ -104,12 +104,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+let firstSum = sum(sumArr[0], sumArr[1]);
+let finalSum = sum(firstSum[0], sumArr[2]);
+// console.log(finalSum);
+let message = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${finalSum[0]} is their sum.`;
+// console.log(message);
+return [finalSum[0], message];
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
