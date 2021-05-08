ac10e17edd873f6118773cba6c68626422c3323d
Phil Murphy <phillipdeanmurphy@gmail.com>
2021-05-08 16:32:21 -0700

    solved problem 5

--- js/app.js
+++ js/app.js
@@ -131,11 +131,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let firstMult = multiply(multArr[0], multArr[1]);
+  let finalMult = multiply(firstMult[0], multArr[2]);
+  let message = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${finalMult[0]}.`;
+  // console.log(message);
+  return [finalMult[0], message];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
