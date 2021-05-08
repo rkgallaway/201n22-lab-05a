00229a25f1c41c037f0403ef834841ef7988ac2e
Heather B <vbchomp@gmail.com>
2021-05-08 14:51:12 -0700

    problem 3:

--- js/app.js
+++ js/app.js
@@ -55,12 +55,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(e, f, g) { //eslint-disable-line
+  let sum31 = sum(e, f)[0];
+  let sum32 = sum(sum31, g)[0];
+  let product31 = multiply(e, f)[0];
+  let product32 = multiply(product31, g)[0];
+  let message31 = `${e} and ${f} and ${g} sum to ${sum32}.`;
+  let message32 = `The product of ${e} and ${f} and ${g} is ${product32}.`;
+  console.log(sum32, product32, message31, message32);
 
+  return [sum32, product32, message31, message32];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
